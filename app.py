917b8664ef52961e57eb3f636b9ea9a29da9d2fa
tatiashafwas <127376255+tatiashafwas@users.noreply.github.com>
2024-11-25 19:32:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ def get_image_base64(image_path):
         return None
 
 # Path ke file logo (ganti sesuai lokasi file Anda)
-logo_uptd_path = r"C:\Users\tatia shafwa s\OneDrive\Documents\Tera Ulang\City_of_Surabaya_Logo.png"
-logo_dinas_path = r"C:\Users\tatia shafwa s\OneDrive\Documents\Tera Ulang\LOGO-METRO.png"
+logo_uptd_path = r"WEB/City_of_Surabaya_Logo.png"
+logo_dinas_path = r"WEB/LOGO-METRO.png"
 
 # Cek keberadaan file logo dan konversi ke Base64
 logo_uptd_base64 = get_image_base64(logo_uptd_path)
